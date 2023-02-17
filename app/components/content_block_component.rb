b074e28cfdac5551f27b6d31e52b28ccfafcae7a
nolan <nolan@stemble.com>
2023-02-17 16:35:59 -0400

    Add a LayoutComponent

--- app/components/content_block_component.rb
+++ app/components/content_block_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class ContentBlockComponent < ViewComponent::Base
-  attr_accessor :class_name
+  attr_accessor :el, :class_name
 
-  def initialize(class_name: '')
+  def initialize(as: :div, class_name: '')
+    @el = as
     @class_name = class_name
   end
 end
