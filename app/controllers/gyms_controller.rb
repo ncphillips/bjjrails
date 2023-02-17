893bdb07c1d9c2cb5ccd06dfea9deafdd3fcb1ce
nolan <nolan@stemble.com>
2023-02-17 16:28:08 -0400

    Gym show.html

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,7 +1,11 @@
 class GymsController < ApplicationController
-  attr_accessor :gyms
+  attr_accessor :gyms, :gym
 
   def index
     @gyms = Gym.all    
+  end
+
+  def show
+    @gym = Gym.find_by(id: params[:id])
   end
 end
