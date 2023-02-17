cd13b1f4a89fcec6b7807ca34b757152e373eb74
nolan <nolan@stemble.com>
2023-02-17 16:55:30 -0400

    Move the action view extension to the lib folder

--- config/application.rb
+++ config/application.rb
@@ -20,18 +20,3 @@ module Bjjrails
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
-
-class ActionView::Base
-  def method_missing(name, *args, **kwargs, &block)
-    begin
-      view_class = Object.const_get(name.to_s.camelize + "Component")
-    rescue StandardError => e
-    end
-
-    if view_class.present?
-      render(view_class.send("new", *args, **kwargs), &block)
-    else
-      super
-    end
-  end
-end
